2028d58c48778b990e0a64f41ac15e8cf04d3f92
unknown <vboogaart@gmail.com>
2022-09-19 19:33:56 -0400

    VB: enlever participants et changer le mot

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -49,7 +49,7 @@ const Header = (props) => {
             <li className = "NavLink"><Link to="/consultation" className = "ReactLink">{content.CONSULTATIONS}</Link></li>
             <li className = "NavLink PhoneRemove"><Link to="/contact" className = "ReactLink">{content.CONTACT}</Link></li>
             <li className = "NavLink"><Link to="/greenBook" className = "ReactLink">{content.GREENBOOK}</Link></li>
-            <li className = "NavLink"><Link to="/participants" className = "ReactLink">{content.PARTNERS}</Link></li>
+            {/* <li className = "NavLink"><Link to="/participants" className = "ReactLink">{content.PARTNERS}</Link></li>*/}
             <li className="LangLink" onClick={e =>props.switchLanguage()}><span className="LanguageButton">{content.LANG}</span></li>
         </ul>
       </nav>
--- Client/src/components/MainBannerContent.jsx
+++ Client/src/components/MainBannerContent.jsx
@@ -3,14 +3,14 @@ import SpecialText from "./SpecialText";
 function MainBannerContent(props) {
     let content = {
         EN: {
-          TEXTE: "Welcome to the Green Outaouais website! The initiative For a green and prosperous Outaouais that I launched in 2021 has turned into a collective effort of your Outaouais elected officials (federal, provincial, MRC and municipal), the CREDDO, association of farmers and entrepreneurs, community organizations, experts and engaged citizens to propose solutions and create, together, a green and prosperous Outaouais by 2040. It's an ambitious initiative, but to quote Nelson Mandela:''it seems impossible until it's done!''.",
+          TEXTE: "Welcome to the Green Outaouais website! I launched the initiative For a green and prosperous Outaouais in 2021, and consulted other Outaouais elected officials, associations of sustainable development, farmers and entrepreneurs, community organizations, experts and engaged citizens to propose solutions and create, together, a green and prosperous Outaouais by 2040. It's an ambitious initiative, but to quote Nelson Mandela: ''it seems impossible until it's done!''.",
           LIEN: "More on the subject...",
           REPTITLE: "       |       MEMBER OF PARLIAMENT FOR PONTIAC"
     
         },
     
         FR: {
-            TEXTE: "Bienvenue sur le site Outaouais vert! L’initiative Pour un Outaouais vert et prospère est un effort collectif de vos élus de l’Outaouais (fédéraux, provinciaux, MRC et municipaux) et du CREDDO, ainsi que des associations d’agriculteurs et d’entrepreneurs, des organismes communautaires, des experts et des citoyens engagés, afin de proposer des solutions et créer, ensemble, un Outaouais vert et prospère d'ici 2040. C’est une initiative ambitieuse, mais comme le disait si bien Nelson Mandela « ça semble impossible jusqu’à ce soit fait ! ».",
+            TEXTE: "Bienvenue sur le site Outaouais vert! L’initiative Pour un Outaouais vert et prospère est une initiative de votre députée fédérale qui a consulté les élus de l’Outaouais, associations de développement durable, d’agriculteurs, d’entrepreneurs, des organismes communautaires, des experts et des citoyens engagés, afin de proposer des solutions et créer, ensemble, un Outaouais vert et prospère. C’est une initiative ambitieuse, mais comme le disait si bien Nelson Mandela « ça semble impossible jusqu’à ce soit fait ! ».",
             LIEN: "En savoir plus...",
             REPTITLE: "       |       DÉPUTÉE DE PONTIAC"
     
